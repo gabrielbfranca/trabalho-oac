54084ba38264979afe5a8ddade5f9c3e746a7119
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2024-06-08 01:00:20 +0000

    teste da nova func

--- exemplo fstream.asm
+++ exemplo fstream.asm
@@ -2,12 +2,23 @@
 
 .data 
 file: .asciiz "teste.asm"
+file2: .asciiz "ArrayList.asm"
 .text
 
-la $a0 file
-openFile WRITE
+#la $a0 file
+#openFile WRITE
+
+#move $a0 $v0
+#la $a1 file
+#writeToFile
+#writeToFile
+
+
+la $a0 file2
+openFile READ
 
 move $a0 $v0
-la $a1 file
-writeToFile
-writeToFile
+readFile
+
+lw $a0 ($v0)
+print_str
